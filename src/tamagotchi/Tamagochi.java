ee9d99fee4bb66613db1e9c191f69bd27eee07a0
AlekseyJava <zvaraleksey@gmail.com>
2020-02-09 23:06:11 +0400

    Update PersonageTamagotchi

--- src/tamagotchi/Tamagochi.java
+++ src/tamagotchi/Tamagochi.java
@@ -2,32 +2,37 @@ package tamagotchi;
 
 import tamagotchi.personTamagotchi.PersonageTamagotchi;
 
-public class Tomagochi {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Date;
 
+public class Tamagochi {
+    public static void main(String[] args) {
         String status; //не родился, жив, умер
+
         PersonageTamagotchi tamagotchi = new PersonageTamagotchi();
         status = tamagotchi.getStatus();
 
 
         //Тамагочи не родился
-        if (status == "select") {
+        if (status.equals("select")) {
             SelectTamagotchi newTamagotchi = new SelectTamagotchi();
             newTamagotchi.select(args);
         }
 
         //Тамагочи живет
-        if (status == "live"){
+        if (status.equals("live")){
             PlayTamagotchi playTamagotchi = new PlayTamagotchi();
             playTamagotchi.buildWindow(args);
         }
 
         //Тамагочи умер
-        if (status == "died"){
+        if (status.equals("died")){
             DiedTamagothci diedTamagothci = new DiedTamagothci();
             diedTamagothci.died(args);
         }
 
 
     }
+
+
 }
--- src/tamagotchi/personTamagotchi/PersonageTamagotchi.java
+++ src/tamagotchi/personTamagotchi/PersonageTamagotchi.java
@@ -1,14 +1,19 @@
 package tamagotchi.personTamagotchi;
 
+import tamagotchi.Tamagochi;
+
+import java.io.*;
+import java.util.Date;
+
 public class PersonageTamagotchi implements Eat, Anger, Play {
-    String status; //завести нового, жив, умер пока нельзя завести нового
-    int statusHunger; //статус насколько голоден
-    int statusSadness;
+    private String status; //завести нового, жив, умер пока нельзя завести нового
+    private int statusHunger; //статус насколько голоден
+    private int statusSadness; //статус насколько печален
+    private String lastFeedingTime; //время последнего кормления
 
     public String getStatus() {
-
         //status = "select";
-        status = "live";
+        //status = "live";
         //status = "died";
 
         //проверка статуса
@@ -17,8 +22,29 @@ public class PersonageTamagotchi implements Eat, Anger, Play {
         // и если менее x часов статус жив
         // от x часов до y часов статус умер
         // больше y часов - статус выбрать
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File("./src//tamagotchi//resource//status.txt")));
+
+            // считаем сначала первую строку
+
+            System.out.println(status = reader.readLine());
+            //System.out.println(lastFeedingTime = reader.readLine());
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return status;
     }
+
+
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 
 
     @Override
@@ -35,5 +61,34 @@ public class PersonageTamagotchi implements Eat, Anger, Play {
     @Override
     public void playWithTamagotchi() {
         System.out.println("Играю с томагочи");
+    }
+
+    public Tamagochi statusTamagochi(){
+        String myTomagochiStatus;
+        String lastFeedingTime;
+        Tamagochi tamagochi = new Tamagochi();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File("./src//status.txt")));
+
+            // считаем сначала первую строку
+
+            System.out.println(myTomagochiStatus = reader.readLine());
+            System.out.println(lastFeedingTime = reader.readLine());
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+        //tamagochi.
+        return tamagochi;
+
     }
 }
