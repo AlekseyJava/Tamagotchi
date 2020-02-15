adb349c9ca055cb2655cacc5b5156090308b225c
AlekseyJava <zvaraleksey@gmail.com>
2020-02-15 11:09:41 +0400

    Update SelectTamagotchi

--- src/main/java/tamagotchi/SelectTamagotchi.java
+++ src/main/java/tamagotchi/SelectTamagotchi.java
@@ -22,24 +22,72 @@ public class SelectTamagotchi extends Application {
         //root.getChildren().addAll(label);
 
         Button buttonDog = new Button("Собака");
-        //buttonDog.setMaxHeight(100);
+        buttonDog.setPrefSize(100,20);
         GridPane.setConstraints(buttonDog, 0,1);
         Label labelDog = new Label("Ест мясо");
         GridPane.setConstraints(labelDog, 1,1);
+
         Button buttonCat = new Button("Кошка");
+        buttonCat.setPrefSize(100,20);
         GridPane.setConstraints(buttonCat, 0,2);
+        Label labelCat = new Label("Ест wiskas");
+        GridPane.setConstraints(labelCat, 1,2);
+
         Button buttonFish = new Button("Рыбка");
+        buttonFish.setPrefSize(100,20);
         GridPane.setConstraints(buttonFish, 0,3);
+        Label labelFish = new Label("Ест fishfood");
+        GridPane.setConstraints(labelFish, 1,3);
+
         Button buttonTurtle = new Button("Черепаха");
+        buttonTurtle.setPrefSize(100,20);
         GridPane.setConstraints(buttonTurtle, 0,4);
+        Label labelTurtle = new Label("Ест все");
+        GridPane.setConstraints(labelTurtle, 1,4);
+
         Button buttonBird = new Button("Птичка");
+        buttonBird.setPrefSize(100,20);
         GridPane.setConstraints(buttonBird, 0,5);
+        Label labelBird = new Label("Ест пшено");
+        GridPane.setConstraints(labelBird, 1,5);
+
+        Label selectLabel = new Label("");
+        GridPane.setConstraints(selectLabel, 1,10);
+        Button selectButton = new Button("Сохранить");
+        GridPane.setConstraints(selectButton, 1,11);
+
+
+        //обрабочик событий
+
+        buttonDog.setOnAction(event -> {
+            selectLabel.setText("Выбрана собака");
+
+        });
+
+        buttonCat.setOnAction(event -> {
+            selectLabel.setText("Выбрана кошка");
+        });
+
+        selectButton.setOnAction(event -> {
+            if(selectLabel.getText().equals("")||(selectLabel.getText().equals("Выберите тамаготчи"))){
+                selectLabel.setText("Выберите тамаготчи");
+            }
+            else{
+                System.out.println("Сохраняем томагочи");
+                //закрываем окно выбора
+                //открываем окно игры
+            }
+
+        });
+
+
 
 
         root.getChildren().addAll(label, buttonDog, buttonCat, buttonFish, buttonTurtle, buttonBird);
-        root.getChildren().addAll(labelDog);
+        root.getChildren().addAll(labelDog, labelCat, labelFish,labelTurtle, labelBird);
+        root.getChildren().addAll(selectLabel, selectButton);
 
-        Scene scene = new Scene(root, 300, 300);
+        Scene scene = new Scene(root, 400, 400);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Окно создания персонажа");
         primaryStage.setResizable(false);
@@ -51,3 +99,4 @@ public class SelectTamagotchi extends Application {
         launch(args);
     }
 }
+
